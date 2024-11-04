713b3c6cb3fc0f0c4b7364be7a69193e5a0daa4b
Giorf81 <rejroberts7@gmail.com>
2024-11-04 12:39:40 +0300

    нвоое

--- 6-5-2/Program.cs
+++ 6-5-2/Program.cs
@@ -6,5 +6,52 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        class figura()
+        {
+            class triangle()
+            {
+                int a;
+                int b;
+                int c;
+                int h;
+
+                public double s()
+                {
+
+                }
+                public int length()
+                {
+
+                }
+            }
+            class square()
+            {
+                int a;
+
+                public int s()
+                {
+
+                }
+                public int length()
+                {
+
+                }
+
+            }
+            class circle()
+            {
+                double rad();
+
+                public double dkinaokr()
+                {
+
+                }
+                public double sokr()
+                {
+
+                }
+            }
+        }
     }
 }
